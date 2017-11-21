beed9416316c06fb4dbd6c96e7b0f9b64df6b9e9
Damian <dtrebilco@gmail.com>
2017-11-21 22:41:51 +1000

    Add initial app files

---
+++ BlendDemo/BlendDemo/App.cpp
@@ -0,0 +1,159 @@
+//======================================================================
+// BlendDemo
+//      By Damian Trebilco
+//======================================================================
+
+#include "App.h"
+#include <crtdbg.h>
+#include <fstream>
+#include <vector>
+#include <string>
+#include <sstream> 
+#include <algorithm>
+#include <time.h>
+
+extern long g_allocRequestCount;
+extern long g_allocFreeCount;
+
+BaseApp *CreateApp() { return new App(); }
+
+bool s_mouseLeftDown = false;
+bool s_mouseRightDown = false;
+int32_t s_mouseDownX = 0;
+int32_t s_mouseDownY = 0;
+
+App::App()
+{
+}
+
+bool App::init()
+{
+  return OpenGLApp::init();
+}
+
+bool App::onKey(const uint key, const bool pressed)
+{
+  if (pressed)
+  {
+    //switch (key)
+    //{
+    //case '1': break;
+    //case '2': break;
+    //case '3': break;
+    //}
+  }
+  return BaseApp::onKey(key, pressed);
+}
+
+bool App::load()
+{
+  // Set the shader version used
+  ((OpenGLRenderer*)renderer)->SetShaderVersionStr("#version 130");
+
+  // Filtering modes
+  if ((trilinearClamp = renderer->addSamplerState(TRILINEAR, CLAMP, CLAMP, CLAMP)) == SS_NONE) return false;
+  if ((trilinearAniso = renderer->addSamplerState(TRILINEAR_ANISO, WRAP, WRAP, WRAP)) == SS_NONE) return false;
+  if ((radialFilter   = renderer->addSamplerState(LINEAR, WRAP, CLAMP, CLAMP)) == SS_NONE) return false;
+
+  //if ((m_perlin = renderer->addTexture("Perlin.png", true, trilinearAniso)) == TEXTURE_NONE) return false;
+  //if ((m_gridDraw = renderer->addShader("gridDraw.shd")) == SHADER_NONE) return false;
+
+  return true;
+}
+
+bool App::onMouseButton(const int x, const int y, const MouseButton button, const bool pressed)
+{
+  if (button == MOUSE_LEFT)
+  {
+    s_mouseLeftDown = pressed;
+    s_mouseDownX = x;
+    s_mouseDownY = y;
+  }
+  if (button == MOUSE_RIGHT)
+  {
+    s_mouseRightDown = pressed;
+    s_mouseDownX = x;
+    s_mouseDownY = y;
+  }
+  return OpenGLApp::onMouseButton(x, y, button, pressed);
+}
+
+bool App::onMouseMove(const int x, const int y, const int deltaX, const int deltaY)
+{
+  if (s_mouseLeftDown ||
+      s_mouseRightDown)
+  {
+    s_mouseDownX = x;
+    s_mouseDownY = y;
+  }
+  return OpenGLApp::onMouseMove(x, y, deltaX, deltaY);
+}
+
+void DrawRoom(uint32_t a_w, uint32_t a_h, uint32_t a_pixelSize)
+{
+  glColor3f(0.0f, 1.0f, 0.0f);
+
+  glBegin(GL_LINES);
+  for (uint32_t i = 0; i <= a_w; i++)
+  {
+    glVertex2i(i * a_pixelSize, 0);
+    glVertex2i(i * a_pixelSize, a_h * a_pixelSize);
+  }
+  for (uint32_t i = 0; i <= a_h; i++)
+  {
+    glVertex2i(0, i * a_pixelSize);
+    glVertex2i(a_w * a_pixelSize, i * a_pixelSize);
+  }
+  glEnd();
+}
+
+void FillBlockPixel(uint32_t a_x, uint32_t a_y, uint32_t a_pixelSize)
+{
+  glVertex2i(a_x, a_y);
+  glVertex2i(a_x + a_pixelSize, a_y);
+
+  glVertex2i(a_x + a_pixelSize, a_y + a_pixelSize);
+  glVertex2i(a_x, a_y + a_pixelSize);
+}
+
+void App::drawFrame()
+{
+  mat4 modelview = scale(1.0f, 1.0f, -1.0f) * rotateXY(-wx, -wy) * translate(-camPos) * rotateX(PI * 0.5f);
+
+  glMatrixMode(GL_PROJECTION);
+  glLoadMatrixf(value_ptr(m_projection));
+
+  glMatrixMode(GL_MODELVIEW);
+  glLoadMatrixf(value_ptr(modelview));
+
+  float clearColor[4] = { 0.0f, 0.0f, 0.0f, 1.0f };
+  renderer->clear(true, true, false, clearColor);
+
+  renderer->setup2DMode(0, (float) width, 0, (float) height);
+
+  // Draw text data to the screen 
+  renderer->reset();
+  //renderer->setShader(m_gridDraw);
+  //renderer->setTexture("perlinTex", m_perlin);
+  renderer->apply();
+
+  renderer->reset();
+  renderer->setDepthState(noDepthWrite);
+  renderer->apply();
+
+  {
+    char buffer[100];
+    float xPos = (float)width - 250.0f;
+
+#ifdef _DEBUG
+    sprintf(buffer, "Alloc Count %d", g_allocRequestCount);
+    renderer->drawText(buffer, xPos, 138.0f, 30, 38, defaultFont, linearClamp, blendSrcAlpha, noDepthTest);
+    
+    sprintf(buffer, "Free Count %d", g_allocFreeCount);
+    renderer->drawText(buffer, xPos, 168.0f, 30, 38, defaultFont, linearClamp, blendSrcAlpha, noDepthTest);
+    
+    sprintf(buffer, "Working Count %d", g_allocRequestCount - g_allocFreeCount);
+    renderer->drawText(buffer, xPos, 198.0f, 30, 38, defaultFont, linearClamp, blendSrcAlpha, noDepthTest);
+#endif // _DEBUG
+  }
+}
---
+++ BlendDemo/BlendDemo/App.h
@@ -0,0 +1,31 @@
+//======================================================================
+// BlendDemo
+//      By Damian Trebilco
+//======================================================================
+
+#include "../Framework3/OpenGL/OpenGLApp.h"
+
+class App : public OpenGLApp {
+public:
+
+  App();
+
+  char *getTitle() const override { return "Blend Demo"; }
+  bool init() override;
+
+	bool onKey(const uint key, const bool pressed) override;
+  bool onMouseButton(const int x, const int y, const MouseButton button, const bool pressed) override;
+	bool onMouseMove(const int x, const int y, const int deltaX, const int deltaY) override;
+
+  bool load() override;
+
+  void drawFrame() override;
+
+protected:
+
+  mat4 m_projection; //!< The projection matrix used
+  SamplerStateID trilinearClamp, trilinearAniso, radialFilter;
+
+  //TextureID m_perlin; 
+  //ShaderID m_gridDraw;
+}; 
