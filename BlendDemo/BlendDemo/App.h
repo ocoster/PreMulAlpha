bc4d6f5908b65675694046b8a5b485372e5a1ac3
Damian <dtrebilco@gmail.com>
2017-11-24 21:01:21 +1000

    Add initial particle class

--- BlendDemo/BlendDemo/App.h
+++ BlendDemo/BlendDemo/App.h
@@ -4,6 +4,7 @@
 //======================================================================
 
 #include "../Framework3/OpenGL/OpenGLApp.h"
+#include <vector>
 
 class App : public OpenGLApp {
 public:
@@ -23,10 +24,23 @@ public:
 
 protected:
 
+  struct Particle
+  {
+    vec2 m_position = vec2(0.0f, 0.0f);
+    vec2 m_direction = vec2(0.0f, 1.0f);
+    float m_alpha = 0.0f;
+    float m_size = 0.0f;
+    float m_rotation = 0.0f;
+    uint8_t m_texType = 0;
+  };
+
+
   mat4 m_projection; //!< The projection matrix used
   SamplerStateID trilinearClamp, trilinearAniso, radialFilter;
 
   int32_t m_divPos = 0;
+
+  std::vector<Particle> m_particles; //!< The array of rendre particles
 
   //TextureID m_perlin; 
   //ShaderID m_gridDraw;
